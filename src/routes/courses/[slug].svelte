0d14a3dd56a3af68f47225f4c10770200e63b7f3
b1mind <brent@1mindsolutions.com>
2022-06-09 14:30:47 -0600

    💨 breaking as we learn

--- src/routes/courses/[slug].svelte
+++ src/routes/courses/[slug].svelte
@@ -1,41 +1,48 @@
 <script context="module">
-	export async function load({ params }) {
-		let slugTitle = params.slug;
-		return { props: { slugTitle } };
-	}
+  export async function load({ params, fetch, session, stuff }) {
+    const url = `/api/${params.slug}.json`
+    const response = await fetch(url)
+    let slugTitle = params.slug
+
+    return {
+      status: response.status,
+      props: {
+        coursesData: response.ok && (await response.json()),
+        slugTitle: slugTitle,
+      },
+    }
+  }
 </script>
 
 <script>
-	import Card from '$lib/Card.svelte';
-	import Footer from '$lib/Footer.svelte';
-	import { courses } from '$lib/data/posts.js';
-	export let slugTitle;
-
-	$: coursesFilter = courses.filter((key) => key.level === slugTitle);
+  import Card from '$lib/Card.svelte'
+  import Footer from '$lib/Footer.svelte'
+  export let slugTitle
+  export let coursesData
 </script>
 
 <section class="container">
-	<h1>{slugTitle}</h1>
-	<div class="grid">
-		{#each coursesFilter as { ...course }}
-			<Card {...course} />
-		{/each}
-	</div>
+  <h1>{slugTitle}</h1>
+  <div class="grid">
+    {#each coursesData as { ...course }}
+      <Card {...course} />
+    {/each}
+  </div>
 
-	{#if coursesFilter.length === 0}
-		<p>There are no {slugTitle} courses open right now.</p>
-	{/if}
+  {#if coursesData.length === 0}
+    <p>There are no {slugTitle} courses open right now.</p>
+  {/if}
 </section>
 
 <Footer />
 
 <style lang="scss">
-	@use '../../lib/scss/vars' as *;
+  @use '../../lib/scss/vars' as *;
 
-	h1 {
-		@media (max-width: $mediaSml) {
-			--fs: clamp(2rem, 10vw, 3.85rem);
-			text-align: center;
-		}
-	}
+  h1 {
+    @media (max-width: $mediaSml) {
+      --fs: clamp(2rem, 10vw, 3.85rem);
+      text-align: center;
+    }
+  }
 </style>
---
+++ src/routes/courses/test/[item].js
@@ -0,0 +1,4 @@
+export async function get({ params }) {
+  let item = 'fuk if this works right'
+  return { body: { item } }
+}
