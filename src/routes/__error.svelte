365f87603baf218838e92daea05fef27f77d173b
b1mind <brent@1mindsolutions.com>
2022-06-11 02:05:03 -0600

    🙈 error template start

---
+++ src/routes/__error.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+  /** @type {import('@sveltejs/kit').Load} */
+  export function load({ error, status }) {
+    return {
+      props: {
+        title: `${status}: ${error.message}`,
+      },
+    }
+  }
+</script>
+
+<script>
+  import Footer from '$lib/Footer.svelte'
+
+  export let title
+</script>
+
+<h1>{title}</h1>
+
+<Footer />
