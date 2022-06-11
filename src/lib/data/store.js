5ea0ee3ac81f8188ebf76db107b7996090c0f16f
b1mind <brent@1mindsolutions.com>
2022-06-10 23:04:44 -0600

    🧹clean up comments

--- src/lib/data/store.js
+++ src/lib/data/store.js
@@ -2,22 +2,3 @@ import { persistStore } from './persistStore'
 const data = []
 
 export let cartStore = persistStore('cart', data)
-// import { writable } from 'svelte/store'
-
-// let courses = [
-//   {
-//     title: 'test',
-//     price: '24.99',
-//   },
-//   {
-//     title: 'another course',
-//     price: '3.99',
-//   },
-//   {
-//     title: 'random name course',
-//     price: '2.99',
-//   },
-// ]
-
-// // export const cartStore = writable(courses)
-// export const cartStore = writable([])
