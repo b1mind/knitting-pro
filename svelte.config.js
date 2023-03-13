5971615c96ec54ed44cda9fc51f405beaf2145e6
b1mind <brent@1mindsolutions.com>
2023-03-12 19:13:40 -0600

    📦 static deploy test

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-node'
-// import adapter from '@sveltejs/adapter-static'
+// import adapter from '@sveltejs/adapter-node'
+import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
